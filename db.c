c3d10ea988455290100b0bd7c3f1ff1519942e87
steperusi <165999263+steperusi@users.noreply.github.com>
2025-02-04 18:26:05 +0100

    Add files via upload

---
+++ db.c
@@ -0,0 +1,499 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <stdbool.h>
+# include <stdint.h>
+# include <io.h>
+# include <windows.h>
+# include <fcntl.h>
+# include <errno.h>
+# define COLUMN_USERNAME_SIZE 32
+# define COLUMN_EMAIL_SIZE 255
+
+# define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+# define ID_SIZE size_of_attribute(Row, id)
+# define USERNAME_SIZE size_of_attribute(Row, username)
+# define EMAIL_SIZE size_of_attribute(Row, email)
+# define ROW_SIZE (ID_SIZE + USERNAME_SIZE + EMAIL_SIZE)
+
+# define ID_OFFSET 0
+# define USERNAME_OFFSET (ID_OFFSET + ID_SIZE)
+# define EMAIL_OFFSET (USERNAME_OFFSET + USERNAME_SIZE)
+# define PAGE_SIZE 4096
+# define TABLE_MAX_PAGES 100
+# define ROWS_PER_PAGE (PAGE_SIZE / ROW_SIZE)
+# define TABLE_MAX_ROWS (ROWS_PER_PAGE * TABLE_MAX_PAGES)
+# define S_IWUSR 0200
+# define S_IRUSR 0400
+#define open _open
+#define close _close
+#define lseek _lseek
+
+
+typedef struct {
+    char* buffer;
+    size_t buffer_length;
+    ssize_t input_length;
+} InputBuffer;
+
+typedef enum {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum {
+    PREPARE_SUCCESS,
+    PREPARE_NEGATIVE_ID,
+    PREPARE_STRING_TOO_LONG,
+    PREPARE_SYNTAX_ERROR,
+    PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+
+typedef enum {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+} StatementType;
+
+typedef enum {
+    EXECUTE_SUCCESS,
+    EXECUTE_TABLE_FULL
+} ExecuteResult;
+
+typedef struct {
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE + 1];
+    char email[COLUMN_EMAIL_SIZE + 1];
+} Row;
+
+typedef struct {
+    StatementType type;
+    Row row_to_insert; // only used by insert statement
+} Statement;
+
+typedef struct {
+    int file_descriptor;
+    uint32_t file_length;
+    void* pages[TABLE_MAX_PAGES];
+} Pager;
+
+typedef struct {
+    Pager* pager;
+    uint32_t num_rows;
+} Table;
+
+// function declarations
+Table* db_open(const char* filename);
+MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table* table);
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement);
+PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement);
+ExecuteResult execute_statement(Statement* statement, Table* table);
+ExecuteResult execute_insert(Statement* statement, Table* table);
+ExecuteResult execute_select(Statement* statement, Table* table);
+void read_input(InputBuffer* input_buffer);
+void print_row(Row* row);
+void print_prompt();
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
+InputBuffer* new_input_buffer();
+void close_input_buffer(InputBuffer* input_buffer);
+void serialize_row(Row* source, void* destination);
+void deserialize_row(void* source, Row* destination);
+void* row_slot(Table* table, uint32_t row_num);
+Pager* pager_open(const char* filename);
+void* get_page(Pager* pager, uint32_t page_num);
+void pager_flush(Pager* pager, uint32_t page_num, uint32_t size);
+void db_close(Table* table);
+void free_table(Table* table);
+
+// function to open the database
+Table* db_open(const char* filename) {
+    Pager* pager = pager_open(filename);
+    uint32_t num_rows = pager->file_length / ROW_SIZE;
+    Table* table = malloc(sizeof(Table));
+    table->pager = pager;
+    table->num_rows = num_rows;
+    return table;
+}
+
+// function to handle meta commands
+MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table* table) {
+    if (strcmp(input_buffer->buffer, ".exit") == 0) {
+        db_close(table);
+        exit(EXIT_SUCCESS);
+    } else {
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
+// function to prepare the statement
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+        return prepare_insert(input_buffer, statement);
+    }
+    if (strcmp(input_buffer->buffer, "select") == 0) {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+// function to execute the statement
+ExecuteResult execute_statement(Statement* statement, Table* table) {
+    switch (statement->type) {
+        case STATEMENT_INSERT:
+            return execute_insert(statement, table);
+        case STATEMENT_SELECT:
+            return execute_select(statement, table);
+    }
+}
+
+// function to print a row
+void print_row(Row* row) {
+    printf("(%d, %s, %s)\n", row->id, row->username, row->email);
+}
+
+// function to create new input buffer
+InputBuffer* new_input_buffer() {
+    InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
+    input_buffer->buffer = NULL;
+    input_buffer->buffer_length = 0;
+    input_buffer->input_length = 0;
+
+    return input_buffer;
+}
+
+// function to print prompt
+void print_prompt() {
+    printf("db > ");
+}
+
+// function to read line of input - it is included for linux, have to make it manual for windows
+ssize_t getline(char **lineptr, size_t *n, FILE *stream) {
+    size_t pos;
+    int c;
+
+    // check if any of the pointers are null
+    if (lineptr == NULL || n == NULL || stream == NULL) {
+        return -1;
+    }
+
+    // read first character from the stream
+    c = fgetc(stream);
+    if (c == EOF /*End Of File*/) {
+        return -1;
+    }
+
+    // if *lineptr is NULL, allocate memory for it
+    if (*lineptr == NULL) {
+        *n = 128; // initial size of the buffer
+        *lineptr = (char *)malloc(*n);
+        if (*lineptr == NULL) {
+            return -1;
+        }
+    }
+
+    pos = 0; // position in the buffer
+    while (c != EOF) {
+        // if the buffer is full, reallocate with larger size
+        if (pos + 1 >= *n) {
+            // increase size by 25%
+            size_t new_size = *n + (*n >> 2 /*shift 2 bits to the right = divide by 4*/);
+            if (new_size < 128) {
+                new_size = 128;
+            }
+            char *new_ptr = (char *)realloc(*lineptr, new_size);
+            if (new_ptr == NULL) {
+                return -1;
+            }
+            *n = new_size;
+            *lineptr = new_ptr;
+        }
+
+        // store the character in the buffer
+        ((unsigned char *)(*lineptr))[pos++] = c;
+        if (c == '\n') {
+            break;
+        }
+        c = fgetc(stream); // read next character
+    }
+
+    // null-terminate the buffer
+    (*lineptr)[pos] = '\0';
+    return pos; // return the number of characters read
+}
+
+// function to read input
+void read_input(InputBuffer* input_buffer) {
+    ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+
+    if (bytes_read <= 0) {
+        printf("Error reading input\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Ignore trailing newline
+    input_buffer->input_length = bytes_read - 1;
+    input_buffer->buffer[bytes_read - 1] = 0;
+}
+
+// function to free the memory of an input buffer
+void close_input_buffer(InputBuffer* input_buffer) {
+    free(input_buffer->buffer);
+    free(input_buffer);
+}
+
+// function to serialize a row
+void serialize_row (Row* source, void* destination) {
+    memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+    memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+    memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+// function to deserialize a row
+void deserialize_row(void *source, Row* destination) {
+    memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+    memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+}
+
+// function to free the memory of a table
+void free_table(Table* table) {
+    Pager* pager = table->pager;
+    for (int i = 0; i < TABLE_MAX_PAGES && pager->pages[i] != NULL; i++) {
+        free(pager->pages[i]);
+    }
+    free(pager);
+    free(table);
+}
+
+// function to get a page from the pager
+void* get_page(Pager* pager, uint32_t page_num) {
+    if (page_num > TABLE_MAX_PAGES) {
+        printf("Tried to fetch page number out of bounds. %d > %d\n", page_num, TABLE_MAX_PAGES);
+        exit(EXIT_FAILURE);
+    }
+
+    if (pager->pages[page_num] == NULL) {
+        // Cache miss. Allocate memory and load from file
+        void* page = malloc(PAGE_SIZE);
+        uint32_t num_pages = pager->file_length / PAGE_SIZE;
+
+        // We might save a partial page at the end of the file
+        if (pager->file_length % PAGE_SIZE) {
+            num_pages += 1;
+        }
+
+        if (page_num <= num_pages) {
+            lseek(pager->file_descriptor, page_num * PAGE_SIZE, SEEK_SET);
+            ssize_t bytes_read = read(pager->file_descriptor, page, PAGE_SIZE);
+            if (bytes_read == -1) {
+                printf("Error reading file: %d\n", errno);
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        pager->pages[page_num] = page;
+    }
+    return pager->pages[page_num];
+}
+
+// function to get a row slot from the table
+void* row_slot(Table* table, uint32_t row_num) {
+    uint32_t page_num = row_num / ROWS_PER_PAGE;
+    void *page = get_page(table->pager, page_num);
+    uint32_t row_offset = row_num % ROWS_PER_PAGE;
+    uint32_t byte_offset = row_offset * ROW_SIZE;
+    return page + byte_offset;
+}
+
+// function to flush a page to the pager
+void pager_flush(Pager* pager, uint32_t page_num, uint32_t size) {
+    if (pager->pages[page_num] == NULL) {
+        printf("Tried to flush null page\n");
+        exit(EXIT_FAILURE);
+    }
+
+    off_t offset = lseek(pager->file_descriptor, page_num * PAGE_SIZE, SEEK_SET);
+    if (offset == -1) {
+        printf("Error seeking: %d\n", errno);
+        exit(EXIT_FAILURE);
+    }
+
+    ssize_t bytes_written = write(pager->file_descriptor, pager->pages[page_num], size);
+    if (bytes_written == -1) {
+        printf("Error writing: %d\n", errno);
+        exit(EXIT_FAILURE);
+    }
+}
+
+// function to close the database
+void db_close (Table* table) {
+    Pager* pager = table->pager;
+    uint32_t num_full_pages = table->num_rows / ROWS_PER_PAGE;
+
+    for (uint32_t i=0; i<num_full_pages; i++) {
+        if (pager->pages[i] == NULL) {
+            continue;
+        }
+        pager_flush(pager, i, PAGE_SIZE);
+        free(pager->pages[i]);
+        pager->pages[i] = NULL;
+    }
+
+    // There may be a partial page to write to the end of the file
+    // This should not be needed after we switch to a B-tree
+    uint32_t num_additional_rows = table->num_rows % ROWS_PER_PAGE;
+    if (num_additional_rows > 0) {
+        uint32_t page_num = num_full_pages;
+        if (pager->pages[page_num] != NULL) {
+            pager_flush(pager, page_num, num_additional_rows * ROW_SIZE);
+            free(pager->pages[page_num]);
+            pager->pages[page_num] = NULL;
+        }
+    }
+
+    int result = close(pager->file_descriptor);
+    if (result == -1) {
+        printf("Error closing db file.\n");
+        exit(EXIT_FAILURE);
+    }
+    for (uint32_t i=0; i<TABLE_MAX_PAGES; i++) {
+        void* page = pager->pages[i];
+        if (page) {
+            free(page);
+            pager->pages[i] = NULL;
+        }
+    }
+    free(pager);
+    free(table);
+}
+
+// function to prepare the insert statement
+PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement) {
+    statement->type = STATEMENT_INSERT;
+    
+    char* keyword = strtok(input_buffer->buffer, " ");
+    char* id_string = strtok(NULL, " ");
+    char* username = strtok(NULL, " ");
+    char* email = strtok(NULL, " ");
+
+    if (id_string == NULL || username == NULL || email == NULL) {
+        return PREPARE_SYNTAX_ERROR;
+    }
+
+    int id = atoi(id_string);
+    if (id < 0) {
+        return PREPARE_NEGATIVE_ID;
+    }
+    if (strlen(username) > COLUMN_USERNAME_SIZE) {
+        return PREPARE_STRING_TOO_LONG;
+    }
+    if (strlen(email) > COLUMN_EMAIL_SIZE) {
+        return PREPARE_STRING_TOO_LONG;
+    }
+
+    statement->row_to_insert.id = id;
+    strcpy(statement->row_to_insert.username, username);
+    strcpy(statement->row_to_insert.email, email);
+
+    return PREPARE_SUCCESS;
+}
+
+// function to execute the insert statement
+ExecuteResult execute_insert(Statement* statement, Table* table) {
+    if (table->num_rows >= TABLE_MAX_ROWS) {
+        return EXECUTE_TABLE_FULL;
+    }
+
+    Row* row_to_insert = &(statement->row_to_insert);
+
+    serialize_row(row_to_insert, row_slot(table, table->num_rows));
+    table->num_rows += 1;
+    return EXECUTE_SUCCESS;
+}
+
+// function to execute the select statement
+ExecuteResult execute_select(Statement* statement, Table* table) {
+    Row row;
+    for (uint32_t i=0; i < table->num_rows; i++) {
+        deserialize_row(row_slot(table, i), &row);
+        print_row(&row);
+    }
+    return EXECUTE_SUCCESS;
+}
+
+// function to open a pager  --- have to fully understand this ---
+Pager* pager_open(const char* filename) {
+    int fd = open(filename,
+                O_RDWR | // read/write mode
+                    O_CREAT, // create file if it does not exist
+                S_IWUSR | // user write permission
+                    S_IRUSR); // user read permission
+    if (fd == -1) {
+        printf("Unable to open file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    off_t file_length = lseek(fd, 0, SEEK_END);
+    Pager* pager = malloc(sizeof(Pager));
+    pager->file_descriptor = fd;
+    pager->file_length = file_length;
+    for (uint32_t i=0; i<TABLE_MAX_PAGES; i++) {
+        pager->pages[i] = NULL;
+    }
+    return pager;
+}
+
+// main function
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        printf("Must supply a database filename.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    char* filename = argv[1];
+    Table* table = db_open(filename);
+
+    InputBuffer* input_buffer = new_input_buffer();
+    while (true) {
+        print_prompt();
+        read_input(input_buffer);
+
+        // all meta commands start with '.' - here I handle them
+        if (input_buffer->buffer[0] == '.') {
+            switch (do_meta_command(input_buffer, table)) {
+                case (META_COMMAND_SUCCESS):
+                    continue;
+                case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                    printf("Unrecognized command '%s'\n", input_buffer->buffer);
+                    continue;
+            }
+        }
+
+        Statement statement;
+        switch (prepare_statement(input_buffer, &statement)) {
+            case (PREPARE_SUCCESS):
+                break;
+            case (PREPARE_NEGATIVE_ID):
+                printf("ID must be positive.\n");
+                continue;
+            case (PREPARE_STRING_TOO_LONG):
+                printf("String is too long.\n");
+                continue;
+            case (PREPARE_SYNTAX_ERROR):
+                printf("Syntax error. Could not parse statement.\n");
+                continue;
+            case (PREPARE_UNRECOGNIZED_STATEMENT):
+                printf("Unrecognized keyword at start of '%s'\n", input_buffer->buffer);
+                continue;
+        }
+
+        switch (execute_statement(&statement, table)) {
+            case (EXECUTE_SUCCESS):
+                printf("Executed.\n");
+                break;
+            case (EXECUTE_TABLE_FULL):
+                printf("Error: Table full.\n");
+                break;
+        }
+    }
+}
